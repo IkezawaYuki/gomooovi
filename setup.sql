544302377d7b57d77bd116bad1b76c8ed9569cc8
yikezawa <y.ikezawa93@gmail.com>
2019-09-26 22:41:01 +0900

    sql

--- setup.sql
+++ setup.sql
@@ -7,11 +7,61 @@ CREATE TABLE users_go(
     created_at TIMESTAMP not null
 );
 
-CREATE TABLE sessions_go (
-  id         serial primary key,
-  uuid       varchar(64) not null unique,
-  email      VARCHAR(255),
-  user_id    integer REFERENCES users(id),
-  created_at timestamp not null
-);
+CREATE TABLE `users_admin_go` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(64) NOT NULL,
+  `nickname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
+
+CREATE TABLE `sessions_go` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(64) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `sessions_admin_go` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `uuid` varchar(64) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id` (`id`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `products` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) DEFAULT NULL,
+  `image_url` text,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `director` varchar(255) DEFAULT NULL,
+  `detail` text,
+  `open_date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_products_on_title` (`title`)
+) ENGINE=InnoDB AUTO_INCREMENT=321 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `reviews_go` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `rate` int(11) DEFAULT NULL,
+  `review` text,
+  `product_id` int(11) DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `author` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8;
